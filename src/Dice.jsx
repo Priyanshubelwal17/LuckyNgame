994bdb1340eb68dc9dfffd90b001234a8fa98fe1
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-01-30 13:29:25 +0530

    start my luckygame with some files

---
+++ src/Dice.jsx
@@ -0,0 +1,13 @@
+import "./Dice.css"
+
+function Dice({ dice }) {
+    return (
+        <section className="Dice" >
+            {dice.map((v, i) => (
+                <Die key={i} val={v} />
+            ))}
+        </section>
+    )
+}
+
+export default Dice;
